d7d8639d16816cd642e721e8e4b7c4bfc7113a99
Sergi_Bogatell_laptop <bogatell.sergi@gmail.com>
2024-11-19 15:22:48 +0100

    Doc creation

---
+++ configureAdress.m
@@ -0,0 +1,10 @@
+function [state] = configureAdress()
+    int state;
+
+end
+
+
+function [state] = configureFreq(freq)
+    int state, freq;
+    
+end
---
+++ readImpedance.m
@@ -0,0 +1,3 @@
+function [magZ, phsZ] = readImpedance
+
+end
